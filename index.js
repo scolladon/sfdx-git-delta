566f26bab5bb718b9fb8825a11357292a7f17fa1
Sébastien Colladon <colladonsebastien@gmail.com>
2019-08-03 02:05:30 +0200

    add prettierrc

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-"use strict";
-const DiffHandler = require("./lib/diffHandler");
-const PackageConstructor = require("./lib/packageConstructor");
-const FileUtils = require("./lib/utils/fileUtils");
+'use strict'
+const DiffHandler = require('./lib/diffHandler')
+const PackageConstructor = require('./lib/packageConstructor')
+const FileUtils = require('./lib/utils/fileUtils')
 
-const DESTRUCTIVE_CHANGES_FILE_NAME = "destructiveChanges.xml";
-const PACKAGE_FILE_NAME = "package.xml";
+const DESTRUCTIVE_CHANGES_FILE_NAME = 'destructiveChanges.xml'
+const PACKAGE_FILE_NAME = 'package.xml'
 
 module.exports = config => {
   return new Promise((resolve, reject) => {
     if (
-      typeof config.to !== "string" ||
-      typeof config.from !== "string" ||
-      typeof config.apiVersion !== "string" ||
-      typeof config.output !== "string" ||
-      typeof config.repo !== "string"
+      typeof config.to !== 'string' ||
+      typeof config.from !== 'string' ||
+      typeof config.apiVersion !== 'string' ||
+      typeof config.output !== 'string' ||
+      typeof config.repo !== 'string'
     ) {
       return reject(
         new Error(
           `Not enough parameter. Execute -h to better understand how to execute`
         )
-      );
+      )
     }
 
-    const diffHandler = new DiffHandler(config);
-    const pc = new PackageConstructor(config);
-    const fu = new FileUtils(config);
+    const diffHandler = new DiffHandler(config)
+    const pc = new PackageConstructor(config)
+    const fu = new FileUtils(config)
 
     diffHandler
       .diff()
@@ -42,10 +42,10 @@ module.exports = config => {
             .constructPackage({})
             .then(emptyPackageContent =>
               fu.writeChangesAsync(emptyPackageContent, PACKAGE_FILE_NAME)
-            )
+            ),
         ])
       )
       .then(() => resolve())
-      .catch(err => reject(err));
-  });
-};
+      .catch(err => reject(err))
+  })
+}
