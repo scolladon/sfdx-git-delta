73a007f18c28d8932b971bb04cbc5684d2f055fc
Sebastien Colladon <colladonsebastien@gmail.com>
2020-03-16 16:57:24 +0100

    100% Coverage for repoGitDiff

--- __tests__/unit/lib/utils/repoGitDiff.test.js
+++ __tests__/unit/lib/utils/repoGitDiff.test.js
@@ -1,5 +1,5 @@
 'use strict'
-const DiffHandler = require('../../../lib/diffHandler')
+const repoGitDiff = require('../../../../lib/utils/repoGitDiff')
 jest.mock('child_process')
 jest.mock('fs-extra')
 jest.mock('fs')
@@ -8,60 +8,38 @@ const mySpawn = require('mock-spawn')()
 require('child_process').spawn = mySpawn
 
 describe(`test if diffHandler`, () => {
-  let diffHandler
-
-  beforeEach(() => {
-    diffHandler = new DiffHandler({ output: '', repo: '' })
-  })
-
   test('can parse git correctly', async () => {
+    const output = []
     mySpawn.setDefault(mySpawn.simple(0, ''))
-    const work = await diffHandler.fullDiff()
-    expect(work.diffs).toStrictEqual({ package: {}, destructiveChanges: {} })
+    const work = await repoGitDiff({ output: '', repo: '' })
+    expect(work).toStrictEqual(output)
   })
 
   test('can resolve destructive change', async () => {
-    mySpawn.setDefault(
-      mySpawn.simple(
-        0,
-        'D      force-app/main/default/objects/Account/fields/awesome.field-meta.xml'
-      )
-    )
-    const work = await diffHandler.fullDiff()
-    expect(work.diffs).toMatchObject({
-      package: {},
-      destructiveChanges: {
-        fields: new Set(['Account.awesome']),
-      },
-    })
+    const output = [
+      'D      force-app/main/default/objects/Account/fields/awesome.field-meta.xml',
+    ]
+    mySpawn.setDefault(mySpawn.simple(0, output[0]))
+    const work = await repoGitDiff({ output: '', repo: '' })
+    expect(work).toMatchObject(output)
   })
 
   test('can resolve file copy when new file is added', async () => {
-    mySpawn.setDefault(
-      mySpawn.simple(
-        0,
-        'A      force-app/main/default/objects/Account/fields/awesome.field-meta.xml'
-      )
-    )
-    const work = await diffHandler.fullDiff()
-    expect(work.diffs).toStrictEqual({
-      package: { fields: new Set(['Account.awesome']) },
-      destructiveChanges: {},
-    })
+    const output = [
+      'A      force-app/main/default/objects/Account/fields/awesome.field-meta.xml',
+    ]
+    mySpawn.setDefault(mySpawn.simple(0, output[0]))
+    const work = await repoGitDiff({ output: '', repo: '' })
+    expect(work).toStrictEqual(output)
   })
 
   test('can resolve file copy when file is modified', async () => {
-    mySpawn.setDefault(
-      mySpawn.simple(
-        0,
-        'M      force-app/main/default/objects/Account/fields/awesome.field-meta.xml'
-      )
-    )
-    const work = await diffHandler.fullDiff()
-    expect(work.diffs).toStrictEqual({
-      package: { fields: new Set(['Account.awesome']) },
-      destructiveChanges: {},
-    })
+    const output = [
+      'M      force-app/main/default/objects/Account/fields/awesome.field-meta.xml',
+    ]
+    mySpawn.setDefault(mySpawn.simple(0, output[0]))
+    const work = await repoGitDiff({ output: '', repo: '' })
+    expect(work).toStrictEqual(output)
   })
 
   test('can reject in case of error', async () => {
@@ -69,7 +47,7 @@ describe(`test if diffHandler`, () => {
     mySpawn.setDefault(mySpawn.simple(1, null, expected))
 
     try {
-      await diffHandler.fullDiff()
+      await repoGitDiff({ output: '', repo: '' })
     } catch (e) {
       expect(e).toBe(expected)
     }
