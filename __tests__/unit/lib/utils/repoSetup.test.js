225862981e6c079fa3ba2200d1e39c83a9e0331e
Sebastien Colladon <colladonsebastien@gmail.com>
2020-03-16 22:57:11 +0100

    100% Coverage for repoSetup

--- __tests__/unit/lib/utils/repoSetup.test.js
+++ __tests__/unit/lib/utils/repoSetup.test.js
@@ -1,5 +1,5 @@
 'use strict'
-const DiffHandler = require('../../../lib/diffHandler')
+const repoSetup = require('../../../../lib/utils/repoSetup')
 jest.mock('child_process')
 jest.mock('fs-extra')
 jest.mock('fs')
@@ -7,71 +7,17 @@ jest.mock('fs')
 const mySpawn = require('mock-spawn')()
 require('child_process').spawn = mySpawn
 
-describe(`test if diffHandler`, () => {
-  let diffHandler
-
-  beforeEach(() => {
-    diffHandler = new DiffHandler({ output: '', repo: '' })
-  })
-
-  test('can parse git correctly', async () => {
+describe(`test if repoSetup`, () => {
+  test('can set config.from if not defined', async () => {
+    const config = { repo: '.' }
     mySpawn.setDefault(mySpawn.simple(0, ''))
-    const work = await diffHandler.fullDiff()
-    expect(work.diffs).toStrictEqual({ package: {}, destructiveChanges: {} })
+    repoSetup(config)
+    expect(config.from).not.toBeUndefined()
   })
-
-  test('can resolve destructive change', async () => {
-    mySpawn.setDefault(
-      mySpawn.simple(
-        0,
-        'D      force-app/main/default/objects/Account/fields/awesome.field-meta.xml'
-      )
-    )
-    const work = await diffHandler.fullDiff()
-    expect(work.diffs).toMatchObject({
-      package: {},
-      destructiveChanges: {
-        fields: new Set(['Account.awesome']),
-      },
-    })
-  })
-
-  test('can resolve file copy when new file is added', async () => {
-    mySpawn.setDefault(
-      mySpawn.simple(
-        0,
-        'A      force-app/main/default/objects/Account/fields/awesome.field-meta.xml'
-      )
-    )
-    const work = await diffHandler.fullDiff()
-    expect(work.diffs).toStrictEqual({
-      package: { fields: new Set(['Account.awesome']) },
-      destructiveChanges: {},
-    })
-  })
-
-  test('can resolve file copy when file is modified', async () => {
-    mySpawn.setDefault(
-      mySpawn.simple(
-        0,
-        'M      force-app/main/default/objects/Account/fields/awesome.field-meta.xml'
-      )
-    )
-    const work = await diffHandler.fullDiff()
-    expect(work.diffs).toStrictEqual({
-      package: { fields: new Set(['Account.awesome']) },
-      destructiveChanges: {},
-    })
-  })
-
-  test('can reject in case of error', async () => {
-    const expected = 'Test Error'
-    mySpawn.setDefault(mySpawn.simple(1, null, expected))
-
-    try {
-      await diffHandler.fullDiff()
-    } catch (e) {
-      expect(e).toBe(expected)
-    }
+  test('can set core.quotepath to off', async () => {
+    const config = { repo: '.', from: 'HEAD' }
+    mySpawn.setDefault(mySpawn.simple(0, ''))
+    repoSetup(config)
+    expect(config.from).not.toBeUndefined()
   })
 })
