6af3b659fa5fec5b5f7b71116155aa2b531706bb
Sebastien <colladonsebastien@gmail.com>
2020-04-14 16:27:41 +0200

    output gracefully (#18)
    * Move package and destructive into their own sub folder
    
    * Fix codeclimate analysis

--- __mocks__/fs-extra.js
+++ __mocks__/fs-extra.js
@@ -1,8 +1,12 @@
 'use strict'
 const fse = jest.genMockFromModule('fs-extra')
 
+fse.errorMode = false
+
 fse.copy = () => new Promise(resolve => resolve())
 
-fse.outputFileSync = () => {}
+fse.outputFileSync = () => {
+  if (fse.errorMode) throw new Error()
+}
 
 module.exports = fse
