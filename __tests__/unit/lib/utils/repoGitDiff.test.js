134aeec0d67e6bd3d2ebb8f3ce361476e58ba474
Sebastien Colladon <colladonsebastien@gmail.com>
2020-09-10 11:10:33 +0200

    Fix linting issues

--- __tests__/unit/lib/utils/repoGitDiff.test.js
+++ __tests__/unit/lib/utils/repoGitDiff.test.js
@@ -4,33 +4,40 @@ const child_process = require('child_process')
 jest.mock('child_process', () => ({ spawnSync: jest.fn() }))
 jest.mock('fs-extra')
 
+const FORCEIGNORE_MOCK_PATH = '__mocks__/.forceignore'
+
 describe(`test if repoGitDiff`, () => {
   test('can parse git correctly', () => {
     const output = []
     child_process.spawnSync.mockImplementation(() => ({
       stdout: '',
     }))
-    // eslint-disable-next-line no-undef
-    const work = repoGitDiff({ output: '', repo: '', ignore: '__tests__/unit/lib/utils/.forceignore' }, globalMetadata)
+    const work = repoGitDiff(
+      { output: '', repo: '', ignore: FORCEIGNORE_MOCK_PATH },
+      // eslint-disable-next-line no-undef
+      globalMetadata
+    )
     expect(work).toStrictEqual(output)
   })
 
   test('can resolve deletion', () => {
-
     const output = [
-      'D      force-app/main/default/objects/Account/fields/awesome.field-meta.xml'
+      'D      force-app/main/default/objects/Account/fields/awesome.field-meta.xml',
     ]
     child_process.spawnSync.mockImplementation(() => ({
       stdout: output[0],
     }))
-    // eslint-disable-next-line no-undef
-    const work = repoGitDiff({ output: '', repo: '', ignore: '__tests__/unit/lib/utils/.forceignore' }, globalMetadata)
+    const work = repoGitDiff(
+      { output: '', repo: '', ignore: FORCEIGNORE_MOCK_PATH },
+      // eslint-disable-next-line no-undef
+      globalMetadata
+    )
     expect(work).toMatchObject(output)
   })
 
   test('can resolve file copy when new file is added', () => {
     const output = [
-      'A      force-app/main/default/objects/Account/fields/awesome.field-meta.xml'
+      'A      force-app/main/default/objects/Account/fields/awesome.field-meta.xml',
     ]
     child_process.spawnSync.mockImplementation(() => ({
       stdout: output[0],
@@ -42,7 +49,7 @@ describe(`test if repoGitDiff`, () => {
 
   test('can resolve file copy when file is modified', () => {
     const output = [
-      'M      force-app/main/default/objects/Account/fields/awesome.field-meta.xml'
+      'M      force-app/main/default/objects/Account/fields/awesome.field-meta.xml',
     ]
     child_process.spawnSync.mockImplementation(() => ({
       stdout: output[0],
@@ -53,14 +60,15 @@ describe(`test if repoGitDiff`, () => {
   })
 
   test('can filter ignored files', () => {
-    const output = [
-      'M      force-app/main/default/lwc/jsconfig.json'
-    ]
+    const output = ['M      force-app/main/default/lwc/jsconfig.json']
     child_process.spawnSync.mockImplementation(() => ({
-      stdout: output[0]
+      stdout: output[0],
     }))
-    // eslint-disable-next-line no-undef
-    const work = repoGitDiff({ output: '', repo: '', ignore: '__tests__/unit/lib/utils/.forceignore' }, globalMetadata)
+    const work = repoGitDiff(
+      { output: '', repo: '', ignore: FORCEIGNORE_MOCK_PATH },
+      // eslint-disable-next-line no-undef
+      globalMetadata
+    )
     //should be empty
     const expected = []
     expect(work).toStrictEqual(expected)
