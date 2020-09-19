cad81371bb6cfa8b774b63ff1a8ccf043aa864be
Sebastien Colladon <colladonsebastien@gmail.com>
2020-09-19 02:01:14 +0200

    Add waveHandlerTest
    Improve testhandler to take an specific expectedType if specified
    Add a clear:cache script

---
+++ __tests__/unit/lib/service/waveHandler.test.js
@@ -0,0 +1,71 @@
+'use strict'
+const WaveHandler = require('../../../../lib/service/waveHandler')
+jest.mock('fs')
+
+const testContext = {
+  handler: WaveHandler,
+  testData: [
+    [
+      'wave',
+      'force-app/main/default/wave/WaveApplicationTest.wapp',
+      new Set(['WaveApplicationTest']),
+      'WaveApplication',
+    ],
+    [
+      'wave',
+      'force-app/main/default/wave/WaveDataflowTest.wdf',
+      new Set(['WaveDataflowTest']),
+      'WaveDataflow',
+    ],
+    [
+      'wave',
+      'force-app/main/default/wave/WaveDashboardTest.wdash',
+      new Set(['WaveDashboardTest']),
+      'WaveDashboard',
+    ],
+    [
+      'wave',
+      'force-app/main/default/wave/WaveDatasetTest.wds',
+      new Set(['WaveDatasetTest']),
+      'WaveDataset',
+    ],
+    [
+      'wave',
+      'force-app/main/default/wave/WaveLensTest.wlens',
+      new Set(['WaveLensTest']),
+      'WaveLens',
+    ],
+    [
+      'wave',
+      'force-app/main/default/wave/WaveRecipeTest.wdpr',
+      new Set(['WaveRecipeTest']),
+      'WaveRecipe',
+    ],
+    [
+      'wave',
+      'force-app/main/default/wave/WaveXmdTest.xmd',
+      new Set(['WaveXmdTest']),
+      'WaveXmd',
+    ],
+  ],
+  work: {
+    config: { output: '', repo: '', generateDelta: true },
+    diffs: { package: {}, destructiveChanges: {} },
+  },
+}
+
+require('fs').__setMockFiles({
+  'force-app/main/default/wave/WaveApplicationTest.wapp': 'test',
+  'force-app/main/default/wave/WaveDataflowTest.wdf': 'test',
+  'force-app/main/default/wave/WaveDashboardTest.wdash': 'test',
+  'force-app/main/default/wave/WaveDatasetTest.wds': 'test',
+  'force-app/main/default/wave/WaveLensTest.wlens': 'test',
+  'force-app/main/default/wave/WaveRecipeTest.wdpr': 'test',
+  'force-app/main/default/wave/WaveXmdTest.xmd': 'test',
+})
+
+// eslint-disable-next-line no-undef
+describe('test WaveHandler', () => {
+  // eslint-disable-next-line no-undef
+  testHandlerHelper(testContext)
+})
