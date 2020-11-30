dc2349efecb18320e2b810daa7239f597bc33a9d
Daniel Galindo <daniel.galindo@orangebank.es>
2020-11-30 14:15:01 +0100

    Code improvement suggested by owner

--- src/service/subFolderElementHandler.js
+++ src/service/subFolderElementHandler.js
@@ -4,10 +4,6 @@ const mc = require('../utils/metadataConstants')
 const path = require('path')
 
 class SubFolderElementHandler extends StandardHandler {
-  handleDeletion() {
-    this._fillPackage(this.diffs.destructiveChanges)
-  }
-
   handleAddition() {
     super.handleAddition()
     if (!this.config.generateDelta) return
@@ -16,15 +12,13 @@ class SubFolderElementHandler extends StandardHandler {
       .slice(this.splittedLine.length - 1)
       .join(path.sep)
 
-    this._copyFiles(
-      path.join(this.config.repo, fileName),
-      path.join(this.config.output, fileName)
-    )
+    if (this.metadata[this.type].metaFile === true) {
+      this._copyFiles(path.join(this.config.repo, fileName + mc.METAFILE_SUFFIX), path.join(this.config.output, fileName + mc.METAFILE_SUFFIX))
+    }
   }
 
-  _fillPackage(packageObject) {
-    packageObject[this.type] = packageObject[this.type] ?? new Set()
-    packageObject[this.type].add(
+  _getParsedPath() {
+    return path.parse(
       this.splittedLine
         .slice(this.splittedLine.length - 1)
         .join(path.sep)
