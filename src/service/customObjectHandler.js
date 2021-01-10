f8647754c0e117a1b8055a2bb97243cd157f0cd9
Sebastien Colladon <colladonsebastien@gmail.com>
2021-01-10 18:17:46 +0100

    Fix issue when there is no field sub folder for objects

--- src/service/customObjectHandler.js
+++ src/service/customObjectHandler.js
@@ -11,27 +11,29 @@ class CustomObjectHandler extends StandardHandler {
   handleAddition() {
     super.handleAddition()
     if (!this.config.generateDelta || this.type !== OBJECT_TYPE) return
-    const fieldsFolder = path.join(
-      path.parse(this.line).dir,
-      mc.FIELD_DIRECTORY_NAME
+    const fieldsFolder = path.resolve(
+      this.config.repo,
+      path.join(path.parse(this.line).dir, mc.FIELD_DIRECTORY_NAME)
     )
-    fs.readdirSync(path.resolve(this.config.repo, fieldsFolder))
-      .filter(fieldPath =>
-        fs
-          .readFileSync(
-            path.resolve(this.config.repo, fieldsFolder, fieldPath),
-            {
-              encoding: gc.UTF8_ENCODING,
-            }
-          )
-          .includes(mc.MASTER_DETAIL_TAG)
-      )
-      .forEach(field =>
-        this._copyFiles(
-          path.resolve(this.config.repo, fieldsFolder, field),
-          path.resolve(this.config.output, fieldsFolder, field)
+    if (fs.existsSync(fieldsFolder)) {
+      fs.readdirSync(fieldsFolder)
+        .filter(fieldPath =>
+          fs
+            .readFileSync(
+              path.resolve(this.config.repo, fieldsFolder, fieldPath),
+              {
+                encoding: gc.UTF8_ENCODING,
+              }
+            )
+            .includes(mc.MASTER_DETAIL_TAG)
         )
-      )
+        .forEach(field =>
+          this._copyFiles(
+            path.resolve(this.config.repo, fieldsFolder, field),
+            path.resolve(this.config.output, fieldsFolder, field)
+          )
+        )
+    }
   }
 }
 
