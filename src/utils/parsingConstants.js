7aad1fe9a30e5d9124413dfda0fa257e85b2a5b3
Sebastien Colladon <colladonsebastien@gmail.com>
2021-01-17 17:04:16 +0100

    Export parsing constant
    Also add arrayMode:true in case of only one element child

---
+++ src/utils/parsingConstants.js
@@ -0,0 +1,15 @@
+'use strict'
+const XML_HEADER = '<?xml version="1.0" encoding="utf-8"?>\n'
+const XML_PARSER_OPTION = {
+  ignoreAttributes: false,
+  ignoreNameSpace: false,
+  arrayMode: true,
+}
+const JSON_PARSER_OPTION = {
+  ...XML_PARSER_OPTION,
+  format: true,
+  indentBy: '    ',
+}
+module.exports.XML_HEADER = XML_HEADER
+module.exports.XML_PARSER_OPTION = XML_PARSER_OPTION
+module.exports.JSON_PARSER_OPTION = JSON_PARSER_OPTION
