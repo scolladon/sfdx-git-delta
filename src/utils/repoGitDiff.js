98ac64ea172e134641218e78f27806f6c68e192e
Sebastien Colladon <colladonsebastien@gmail.com>
2021-02-28 19:21:55 +0100

    Improve filtering for moved files
    Avoid creating destructiveChanges.xml entry for
    Renamed path (and possibly changed in the meantime) files

--- src/utils/repoGitDiff.js
+++ src/utils/repoGitDiff.js
@@ -24,12 +24,33 @@ module.exports = (config, metadata) => {
   return treatResult(cpUtils.treatDataFromSpawn(diff), metadata)
 }
 
-const treatResult = (repoDiffResult, metadata) =>
-  repoDiffResult
-    .split(os.EOL)
-    .filter(line => !!line && !ig.ignores(line.replace(/^.\s+/u, '')))
-    .filter(line =>
+const treatResult = (repoDiffResult, metadata) => {
+  const lines = repoDiffResult.split(os.EOL),
+    AD = {
+      A: [],
+      D: [],
+    }
+
+  lines.forEach(line => AD[line.charAt(0)]?.push(line))
+
+  const AfileName = new Set(
+      AD.A.map(
+        line => path.parse(line.replace(gc.GIT_DIFF_TYPE_REGEX, '')).base
+      )
+    ),
+    moved = new Set(
+      AD.D.filter(line =>
+        AfileName.has(path.parse(line.replace(gc.GIT_DIFF_TYPE_REGEX, '')).base)
+      )
+    )
+
+  return lines.filter(
+    line =>
+      !!line &&
+      !moved.has(line) &&
+      !ig.ignores(line.replace(gc.GIT_DIFF_TYPE_REGEX, '')) &&
       line
         .split(path.sep)
         .some(part => Object.prototype.hasOwnProperty.call(metadata, part))
-    )
+  )
+}
