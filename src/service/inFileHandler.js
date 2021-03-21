scolladon/sfdx-git-delta

commit b85bb25a09645dbcb3d4116769b7d3cb971e001c

Workaround SharingRule and Workflow deployability (#102)
* Add the parent type for Workflow and SharingRule sub type

By calling the parent method to add element
Only if not a custom label type (this type is tricky)
CustomLabel could be refactored in their own handler

* Fix issue when CustomLabel file is added

It generated a false package.xml with false type for custom label

* Add test case scenario

* Improve test case scenario for CustomLabels

* Add more precise commands for execution context

src/service/inFileHandler.js
@@ -37,7 +37,9 @@ class InFileHandler extends StandardHandler {
   }
 
   handleAddition() {
-    super.handleAddition()
+    if (this.type !== mc.LABEL_EXTENSION) {
+      super.handleAddition()
+    }
     this._fillPackageFromFile(this.diffs.package)
   }
 
@@ -46,6 +48,9 @@ class InFileHandler extends StandardHandler {
   }
 
   handleModification() {
+    if (this.type !== mc.LABEL_EXTENSION) {
+      super.handleAddition()
+    }
     const toAdd = this._handleInFile()
     this._handleFileWriting(toAdd)
   }

__tests__/unit/lib/service/inFileHandler.test.js
@@ -103,6 +103,11 @@ describe(`test if inFileHandler`, () => {
         expect(work.diffs.package).toMatchObject(
           testContext.expectedData[expectedType]
         )
+        if (expectedType === mc.LABEL_EXTENSION) {
+          expect(work.diffs.package).not.toHaveProperty(expectedType)
+        } else {
+          expect(work.diffs.package).toHaveProperty(expectedType)
+        }
       })
       test('deletion', () => {
         const work = {
@@ -127,7 +132,9 @@ describe(`test if inFileHandler`, () => {
           testContext.expectedData[expectedType]
         )
         expect(work.diffs.destructiveChanges).not.toHaveProperty('workflows')
-        expect(work.diffs.destructiveChanges).not.toHaveProperty('labels')
+        expect(work.diffs.destructiveChanges).not.toHaveProperty(
+          mc.LABEL_EXTENSION
+        )
         expect(work.diffs.destructiveChanges).not.toHaveProperty('sharingRules')
       })
       test('modification', () => {
@@ -154,6 +161,11 @@ describe(`test if inFileHandler`, () => {
 
         expect(work.diffs.package).toBeDefined()
         expect(work.diffs.destructiveChanges).toBeDefined()
+        if (expectedType === mc.LABEL_EXTENSION) {
+          expect(work.diffs.package).not.toHaveProperty(expectedType)
+        } else {
+          expect(work.diffs.package).toHaveProperty(expectedType)
+        }
       })
 
       test('modification without delta generation', () => {
@@ -178,6 +190,11 @@ describe(`test if inFileHandler`, () => {
         expect(work.diffs.package).toMatchObject(
           testContext.expectedData[expectedType]
         )
+        if (expectedType === mc.LABEL_EXTENSION) {
+          expect(work.diffs.package).not.toHaveProperty(expectedType)
+        } else {
+          expect(work.diffs.package).toHaveProperty(expectedType)
+        }
       })
     }
   )

src/utils/metadataConstants.js
@@ -4,7 +4,8 @@ const META_REGEX = new RegExp(`${METAFILE_SUFFIX}$`)
 const OBJECT_META_XML_SUFFIX = `object${METAFILE_SUFFIX}`
 const MASTER_DETAIL_TAG = '<type>MasterDetail</type>'
 const FIELD_DIRECTORY_NAME = 'fields'
-const LABEL_DIRECTORY_NAME = 'labels.labels'
+const LABEL_EXTENSION = 'labels'
+const LABEL_DIRECTORY_NAME = `labels.${LABEL_EXTENSION}`
 const OBJECT_TRANSLATION_META_XML_SUFFIX = `objectTranslation${METAFILE_SUFFIX}`
 const INFOLDER_SUFFIX = `Folder`
 const WAVE_SUB_TYPES_PREFIX = 'Wave'
@@ -13,6 +14,7 @@ module.exports.META_REGEX = META_REGEX
 module.exports.OBJECT_META_XML_SUFFIX = OBJECT_META_XML_SUFFIX
 module.exports.MASTER_DETAIL_TAG = MASTER_DETAIL_TAG
 module.exports.FIELD_DIRECTORY_NAME = FIELD_DIRECTORY_NAME
+module.exports.LABEL_EXTENSION = LABEL_EXTENSION
 module.exports.LABEL_DIRECTORY_NAME = LABEL_DIRECTORY_NAME
 module.exports.INFOLDER_SUFFIX = INFOLDER_SUFFIX
 module.exports.OBJECT_TRANSLATION_META_XML_SUFFIX = OBJECT_TRANSLATION_META_XML_SUFFIX

commit 0b2a59ba6cec6bb73fc3730ae656332c078751b0

Fix CustomLabels handling
Override the _fillPackage function
in order to apply template method pattern and benefit from parent func
This issue comes from a regression inserted by #102

src/service/inFileHandler.js
@@ -36,9 +36,7 @@ class InFileHandler extends StandardHandler {
   }
 
   handleAddition() {
-    if (this.type !== mc.LABEL_EXTENSION) {
-      super.handleAddition()
-    }
+    super.handleAddition()
     this._fillPackageFromFile(this.diffs.package)
   }
 
@@ -47,9 +45,7 @@ class InFileHandler extends StandardHandler {
   }
 
   handleModification() {
-    if (this.type !== mc.LABEL_EXTENSION) {
-      super.handleAddition()
-    }
+    super.handleAddition()
     const toAdd = this._handleInFile()
     this._handleFileWriting(toAdd)
   }
@@ -129,6 +125,12 @@ class InFileHandler extends StandardHandler {
     return {
       authorizedKeys: authorizedKeys,
       fileContent: result,
+    }
+  }
+
+  _fillPackage(packageObject) {
+    if (this.type !== mc.LABEL_EXTENSION) {
+      super._fillPackage(packageObject)
     }
   }
 