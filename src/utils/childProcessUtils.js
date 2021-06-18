15b5f401422e4f0177fb05a5f47669f5e595e9ee
Sebastien Colladon <colladonsebastien@gmail.com>
2021-06-18 16:31:42 +0200

    fix: sanitize path input
    improve portability of the code

--- src/utils/childProcessUtils.js
+++ src/utils/childProcessUtils.js
@@ -4,5 +4,8 @@ const path = require('path')
 
 const treatEOL = data => data.replace(/\r?\n/g, os.EOL)
 const treatPathSep = data => data.replace(/[/\\]+/g, path.sep)
+const sanitizePath = data => path.normalize(treatPathSep(data))
+
 module.exports.treatDataFromSpawn = data => treatEOL(treatPathSep(data))
 module.exports.treatEOL = treatEOL
+module.exports.sanitizePath = sanitizePath
