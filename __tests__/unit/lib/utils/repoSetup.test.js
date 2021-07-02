f2f473384d1cc10b9a422a077cc81a0a940d3813
Sebastien Colladon <colladonsebastien@gmail.com>
2021-07-02 19:26:05 +0200

    test: test new methods

--- __tests__/unit/lib/utils/repoSetup.test.js
+++ __tests__/unit/lib/utils/repoSetup.test.js
@@ -1,5 +1,5 @@
 'use strict'
-const repoSetup = require('../../../../src/utils/repoSetup')
+const RepoSetup = require('../../../../src/utils/repoSetup')
 const child_process = require('child_process')
 jest.mock('child_process', () => ({ spawnSync: jest.fn() }))
 jest.mock('fs-extra')
@@ -11,15 +11,39 @@ describe(`test if repoSetup`, () => {
     child_process.spawnSync.mockImplementation(() => ({
       stdout: '',
     }))
-    repoSetup(config)
-    expect(config.from).not.toBeUndefined()
+    const repoSetup = new RepoSetup(config)
+    const firsSha = repoSetup.getFirstSHA()
+
+    expect(firsSha).not.toBeUndefined()
+    expect(child_process.spawnSync).toHaveBeenCalled()
   })
   test('can set core.quotepath to off', () => {
     const config = { repo: '.', from: 'HEAD' }
     child_process.spawnSync.mockImplementation(() => ({
       stdout: '',
     }))
-    repoSetup(config)
-    expect(config.from).not.toBeUndefined()
+    const repoSetup = new RepoSetup(config)
+    repoSetup.repoConfiguration()
+    expect(child_process.spawnSync).toHaveBeenCalled()
+  })
+
+  test('can checkout to', () => {
+    const config = { repo: '.', from: 'HEAD' }
+    child_process.spawnSync.mockImplementation(() => ({
+      stdout: '',
+    }))
+    const repoSetup = new RepoSetup(config)
+    repoSetup.checkoutTo()
+    expect(child_process.spawnSync).toHaveBeenCalled()
+  })
+
+  test('can checkout back', () => {
+    const config = { repo: '.', from: 'HEAD' }
+    child_process.spawnSync.mockImplementation(() => ({
+      stdout: '',
+    }))
+    const repoSetup = new RepoSetup(config)
+    repoSetup.checkoutRef()
+    expect(child_process.spawnSync).toHaveBeenCalled()
   })
 })
