d751f2f8ab856b10c60cf754af45a39dcae14a0c
Sebastien Colladon <colladonsebastien@gmail.com>
2021-07-29 00:57:09 +0200

    refactor: improve code quality

--- src/service/customObjectHandler.js
+++ src/service/customObjectHandler.js
@@ -5,37 +5,41 @@ const mc = require('../utils/metadataConstants')
 const path = require('path')
 const fs = require('fs')
 
+const readFileSyncOptions = {
+  encoding: gc.UTF8_ENCODING,
+}
+
 class CustomObjectHandler extends StandardHandler {
   handleAddition() {
     super.handleAddition()
-    if (
-      !this.config.generateDelta ||
-      this.type !== CustomObjectHandler.OBJECT_TYPE
-    )
-      return
+    if (!this.config.generateDelta) return
+    this._handleMasterDetailException()
+  }
+
+  _handleMasterDetailException() {
+    if (this.type !== CustomObjectHandler.OBJECT_TYPE) return
+
     const fieldsFolder = path.resolve(
       this.config.repo,
       path.join(path.parse(this.line).dir, mc.FIELD_DIRECTORY_NAME)
     )
-    if (fs.existsSync(fieldsFolder)) {
-      fs.readdirSync(fieldsFolder)
-        .filter(fieldPath =>
-          fs
-            .readFileSync(
-              path.resolve(this.config.repo, fieldsFolder, fieldPath),
-              {
-                encoding: gc.UTF8_ENCODING,
-              }
-            )
-            .includes(mc.MASTER_DETAIL_TAG)
-        )
-        .forEach(field =>
-          this._copyFiles(
-            path.resolve(this.config.repo, fieldsFolder, field),
-            path.resolve(this.config.output, fieldsFolder, field)
+    if (!fs.existsSync(fieldsFolder)) return
+
+    fs.readdirSync(fieldsFolder)
+      .filter(fieldPath =>
+        fs
+          .readFileSync(
+            path.resolve(this.config.repo, fieldsFolder, fieldPath),
+            readFileSyncOptions
           )
+          .includes(mc.MASTER_DETAIL_TAG)
+      )
+      .forEach(field =>
+        this._copyFiles(
+          path.resolve(this.config.repo, fieldsFolder, field),
+          path.resolve(this.config.output, fieldsFolder, field)
         )
-    }
+      )
   }
 
   static OBJECT_TYPE = 'objects'
