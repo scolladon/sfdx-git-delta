01c8b6e1600b1bfbf551db98c9f7ee6f331e3696
Sébastien Colladon <scolladon@salesforce.com>
2021-08-30 09:06:45 +0200

    feat: add permissive diff to inFile git comparison

--- src/utils/fileGitDiff.js
+++ src/utils/fileGitDiff.js
@@ -6,9 +6,19 @@ const gc = require('./gitConstants')
 const unitDiffParams = ['--no-pager', 'diff', '--no-prefix', '-U200']
 
 module.exports = (filePath, config) => {
+  const permissiveDiffParams = config.permissiveDiff
+    ? gc.PERMISSIVE_DIFF_PARAMS
+    : []
   const { stdout: diff } = childProcess.spawnSync(
     'git',
-    [...unitDiffParams, config.from, config.to, '--', filePath],
+    [
+      ...unitDiffParams,
+      ...permissiveDiffParams,
+      config.from,
+      config.to,
+      '--',
+      filePath,
+    ],
     { cwd: config.repo, encoding: gc.UTF8_ENCODING }
   )
 
