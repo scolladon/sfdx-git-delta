0a3d2a949d155829f9eea4f0a7168fd75bf2929a
Sebastien Colladon <colladonsebastien@gmail.com>
2022-02-18 23:36:51 +0100

    test: fix fileGitDiff.test

--- __tests__/unit/lib/utils/fileGitDiff.test.js
+++ __tests__/unit/lib/utils/fileGitDiff.test.js
@@ -1,76 +1,75 @@
 'use strict'
 const fileGitDiff = require('../../../../src/utils/fileGitDiff')
+jest.mock('child_process')
 const child_process = require('child_process')
-jest.mock('child_process', () => ({ spawnSync: jest.fn() }))
-jest.mock('fs-extra')
-jest.mock('fs')
 
 const TEST_PATH = 'path/to/file'
 
 describe(`test if fileGitDiff`, () => {
-  test('can parse git diff header', () => {
-    const output = '@git diff'
-    child_process.spawnSync.mockImplementation(() => ({
-      stdout: output,
-    }))
+  beforeEach(() => {
+    child_process.__setOutput([])
+    child_process.__setError(false)
+  })
+  test('can parse git diff header', async () => {
+    const output = ['@git diff']
+    child_process.__setOutput([output])
     const result = fileGitDiff(TEST_PATH, { output: '', repo: '' })
-    expect(result).toStrictEqual(output)
+    for await (const line of result) {
+      expect(line).toStrictEqual(output.shift())
+    }
   })
 
-  test('can parse git diff addition', () => {
-    const output = '+ line added'
+  test('can parse git diff addition', async () => {
+    const output = ['+ line added']
 
-    child_process.spawnSync.mockImplementation(() => ({
-      stdout: output,
-    }))
-    const work = fileGitDiff(TEST_PATH, { output: '', repo: '' })
-    expect(work).toStrictEqual(output)
+    child_process.__setOutput([output])
+    const result = fileGitDiff(TEST_PATH, { output: '', repo: '' })
+    for await (const line of result) {
+      expect(line).toStrictEqual(output.shift())
+    }
   })
 
-  test('can parse git diff deletion', () => {
-    const output = '- line deleted'
+  test('can parse git diff deletion', async () => {
+    const output = ['- line deleted']
 
-    child_process.spawnSync.mockImplementation(() => ({
-      stdout: output,
-    }))
-    const work = fileGitDiff(TEST_PATH, { output: '', repo: '' })
-    expect(work).toStrictEqual(output)
+    child_process.__setOutput([output])
+    const result = fileGitDiff(TEST_PATH, { output: '', repo: '' })
+    for await (const line of result) {
+      expect(line).toStrictEqual(output.shift())
+    }
   })
 
-  test('can apply permissive git diff', () => {
-    const output = 'diff'
+  test('can apply permissive git diff', async () => {
+    const output = ['diff']
 
-    child_process.spawnSync.mockImplementation(() => ({
-      stdout: output,
-    }))
-    const work = fileGitDiff(TEST_PATH, {
+    child_process.__setOutput([output])
+    const result = fileGitDiff(TEST_PATH, {
       output: '',
       repo: '',
       ignoreWhitespace: true,
     })
-    expect(work).toStrictEqual(output)
+    for await (const line of result) {
+      expect(line).toStrictEqual(output.shift())
+    }
   })
 
-  test('can parse git diff context line', () => {
-    const output = 'context line'
+  test('can parse git diff context line', async () => {
+    const output = ['context line']
 
-    child_process.spawnSync.mockImplementation(() => ({
-      stdout: output,
-    }))
-    const work = fileGitDiff(TEST_PATH, { output: '', repo: '' })
-    expect(work).toStrictEqual(output)
+    child_process.__setOutput([output])
+    const result = fileGitDiff(TEST_PATH, { output: '', repo: '' })
+    for await (const line of result) {
+      expect(line).toStrictEqual(output.shift())
+    }
   })
 
   test('can reject in case of error', () => {
-    const expected = new Error('Test Error')
-    child_process.spawnSync.mockImplementation(() => {
-      throw expected
-    })
+    child_process.__setError(true)
 
     try {
       fileGitDiff(TEST_PATH, { output: '', repo: '' })
     } catch (e) {
-      expect(e).toBe(expected)
+      expect(e).toBeDefined()
     }
   })
 })
