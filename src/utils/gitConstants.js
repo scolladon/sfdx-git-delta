3090b606105313f1ed04bab6e2ffe3a738c484d5
Sebastien Colladon <colladonsebastien@gmail.com>
2022-03-16 00:27:51 +0100

    feat: add default `--word-diff-regex`

--- src/utils/gitConstants.js
+++ src/utils/gitConstants.js
@@ -7,7 +7,7 @@ module.exports.IGNORE_WHITESPACE_PARAMS = [
   '--ignore-all-space',
   '--ignore-blank-lines',
   '--ignore-cr-at-eol',
-  '--word-diff-regex',
+  '--word-diff-regex=|[^[:space:]]|[\xc0-\xff][\x80-\xbf]+',
 ]
 module.exports.PLUS = '+'
 module.exports.UTF8_ENCODING = 'utf8'
