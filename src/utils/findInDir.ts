276e6f1fc36101b17dc2a25e16106626816e56fb
Sebastien Colladon <colladonsebastien@gmail.com>
2022-07-18 18:42:16 +0200

    fix: prettify the file

--- src/utils/findInDir.ts
+++ src/utils/findInDir.ts
@@ -1,21 +1,25 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from 'fs'
+import * as path from 'path'
 
-const findInDir = (dir: string, filter: RegExp, fileList: string[] = []): string[] => {
-    const files = fs.readdirSync(dir);
+const findInDir = (
+  dir: string,
+  filter: RegExp,
+  fileList: string[] = []
+): string[] => {
+  const files = fs.readdirSync(dir)
 
-    files.forEach(file => {
-        const filePath = path.join(dir, file);
-        const fileStat = fs.lstatSync(filePath);
+  files.forEach(file => {
+    const filePath = path.join(dir, file)
+    const fileStat = fs.lstatSync(filePath)
 
-        if (fileStat.isDirectory()) {
-            findInDir(filePath, filter, fileList);
-        } else if (filter.test(filePath)) {
-            fileList.push(filePath);
-        }
-    });
+    if (fileStat.isDirectory()) {
+      findInDir(filePath, filter, fileList)
+    } else if (filter.test(filePath)) {
+      fileList.push(filePath)
+    }
+  })
 
-    return fileList;
-};
+  return fileList
+}
 
-export { findInDir };
+export { findInDir }
