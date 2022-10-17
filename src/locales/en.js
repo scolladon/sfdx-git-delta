de10c1d1b5084eeb6f9212e5e8d83cb4baed1078
Sebastien Colladon <colladonsebastien@gmail.com>
2022-10-17 09:41:21 +0200

    refactor: remove double quote from json output

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,10 +1,10 @@
 module.exports = {
-  errorGitSHAisBlank: '--%s is blank: "%s"',
-  errorParameterIsNotGitSHA: '--%s is not a valid sha pointer: "%s"',
-  errorPathIsNotDir: '%s folder does not exist',
-  errorPathIsNotFile: '%s file does not exist',
-  errorPathIsNotGit: '%s is not a git repository',
+  errorGitSHAisBlank: `--%s is blank: '%s'`,
+  errorParameterIsNotGitSHA: `--%s is not a valid sha pointer: '%s'`,
+  errorPathIsNotDir: `'%s' folder does not exist`,
+  errorPathIsNotFile: `'%s' file does not exist`,
+  errorPathIsNotGit: `'%s' is not a git repository`,
   errorToNotHeadWithDeltaGenerate:
     '--generate-delta (-d) parameter cannot be used when --to (-t) parameter is not equivalent to HEAD',
-  warningApiVersionNotSupported: 'API version not supported, using %s instead',
+  warningApiVersionNotSupported: `API version not supported, using '%s' instead`,
 }
