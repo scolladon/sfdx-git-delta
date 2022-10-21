d0d1b6e48667220f0699b8a29d890be63855f717
Sebastien <colladonsebastien@gmail.com>
2022-10-21 09:08:37 +0200

    feat: improve warning message
    Co-authored-by: Mehdi Cherfaoui <mcherfaoui@salesforce.com>

--- src/locales/en.js
+++ src/locales/en.js
@@ -6,5 +6,5 @@ module.exports = {
   errorPathIsNotGit: `'%s' is not a git repository`,
   errorToNotHeadWithDeltaGenerate:
     '--generate-delta (-d) parameter cannot be used when --to (-t) parameter is not equivalent to HEAD',
-  warningApiVersionNotSupported: `API version not supported, using '%s' instead`,
+  warningApiVersionNotSupported: `API version not found or not supported, using '%s' instead`,
 }
