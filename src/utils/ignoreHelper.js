9c48a1a6da1c359483c27d43c3acee2ac8b37014
Sebastien Colladon <colladonsebastien@gmail.com>
2022-11-10 11:34:52 +0100

    fix: ignore destructive with ignore when possible

--- src/utils/ignoreHelper.js
+++ src/utils/ignoreHelper.js
@@ -1,19 +1,55 @@
 const ignore = require('ignore')
 const { readFile } = require('../utils/fsHelper')
-
+const {
+  ADDITION,
+  DELETION,
+  MODIFICATION,
+  GIT_DIFF_TYPE_REGEX,
+} = require('./gitConstants')
 class IgnoreHelper {
-  static ignorePerPath = new Map()
+  globalIgnore
+  destructiveIgnore
 
-  async forPath(ignorePath) {
-    if (!IgnoreHelper.ignorePerPath.has(ignorePath)) {
-      const ign = ignore()
-      const content = await readFile(ignorePath)
-      ign.add(content.toString())
-      IgnoreHelper.ignorePerPath.set(ignorePath, ign)
+  constructor(globalIgnore, destructiveIgnore) {
+    this.globalIgnore = globalIgnore
+    this.destructiveIgnore = destructiveIgnore ?? globalIgnore
+  }
+
+  keep(line) {
+    const changeType = line.charAt(0)
+
+    let ignInstance = null
+    if (DELETION == changeType) {
+      ignInstance = this.destructiveIgnore
+    } else if ([ADDITION, MODIFICATION].includes(changeType)) {
+      ignInstance = this.globalIgnore
     }
 
-    return IgnoreHelper.ignorePerPath.get(ignorePath)
+    const filePath = line.replace(GIT_DIFF_TYPE_REGEX, '')
+
+    return !ignInstance?.ignores(filePath)
   }
 }
 
-module.exports = IgnoreHelper
+const buildIgnoreHelper = async config => {
+  const globalIgnore = await forPath(config.ignore)
+  const destructiveIgnore = await forPath(config.ignoreDestructive)
+
+  return new IgnoreHelper(globalIgnore, destructiveIgnore)
+}
+
+const ignorePerPath = new Map()
+const forPath = async ignorePath => {
+  let ign = null
+  if (ignorePath && !ignorePerPath.has(ignorePath)) {
+    ign = ignore()
+    const content = await readFile(ignorePath)
+    ign.add(content.toString())
+    ignorePerPath.set(ignorePath, ign)
+  }
+
+  return ignorePerPath.get(ignorePath)
+}
+
+module.exports.forPath = forPath
+module.exports.buildIgnoreHelper = buildIgnoreHelper
