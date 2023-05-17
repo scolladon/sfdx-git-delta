ec5bc800b8d3fe2656bdb32787641e9099ad32a1
Sébastien Colladon <colladonsebastien@gmail.com>
2023-05-17 17:17:55 +0200

    test: add NUT tests for `--json` success|error use cases

--- __tests__/functional/delta.nut.ts
+++ __tests__/functional/delta.nut.ts
@@ -9,4 +9,36 @@ describe('sgd:source:delta NUTS', () => {
     }).shellOutput
     expect(result).to.include('sgd:source:delta')
   })
+
+  it('detects not existing output folder', () => {
+    const result = execCmd('sgd:source:delta --from "HEAD"', {
+      ensureExitCode: 1,
+    }).shellOutput
+    expect(result).to.include('folder does not exist')
+    expect(result).to.include('"success": false')
+  })
+
+  it('detects not existing output folder with json outputs', () => {
+    const result = execCmd('sgd:source:delta --from "HEAD" --json', {
+      ensureExitCode: 1,
+    }).shellOutput
+    expect(result).to.include('folder does not exist')
+    expect(result).to.include('"success": false')
+  })
+
+  it('outputs json', () => {
+    const result = execCmd('sgd:source:delta --from "HEAD" -o reports', {
+      ensureExitCode: 0,
+    }).shellOutput
+    expect(result).to.include('"error": null')
+    expect(result).to.include('"success": true')
+  })
+
+  it('outputs json with `--json`', () => {
+    const result = execCmd('sgd:source:delta --from "HEAD" -o reports --json', {
+      ensureExitCode: 0,
+    }).shellOutput
+    expect(result).to.include('"error": null')
+    expect(result).to.include('"success": true')
+  })
 })
