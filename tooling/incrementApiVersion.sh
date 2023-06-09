f4a5b3e8830f3374389b860e8815e4fba2b9d1ce
Sébastien Colladon <colladonsebastien@gmail.com>
2023-06-09 14:25:38 +0200

    fix: shell script issue

--- tooling/incrementApiVersion.sh
+++ tooling/incrementApiVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-filename=`ls src/metadata/v*.json | tail -1`
-version=`echo $filename | tr -d -c 0-9`
+filename=`find src/metadata -type f -name "v*.json" | sort | tail -1`
+version=`echo "$filename" | tr -d -c 0-9`
 ((version++))
 targetname="src/metadata/v${version}.json"
-\cp $filename $targetname
+\cp "$filename" "$targetname"
