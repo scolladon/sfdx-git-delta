47e8850d0836ae72202e07c9268a159cfff829eb
github-action-benchmark <github@users.noreply.github.com>
2023-07-24 11:09:19 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 0f8a869b0f7d07dbd28d04c0222bbae2377f9758

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1690196959108,
+  "repoUrl": "https://github.com/scolladon/sfdx-git-delta",
+  "entries": {
+    "Benchmark.js Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "colladonsebastien@gmail.com",
+            "name": "Sebastien",
+            "username": "scolladon"
+          },
+          "committer": {
+            "email": "colladonsebastien@gmail.com",
+            "name": "Sébastien Colladon",
+            "username": "scolladon"
+          },
+          "distinct": true,
+          "id": "0f8a869b0f7d07dbd28d04c0222bbae2377f9758",
+          "message": "ci: add performance testing (#633)",
+          "timestamp": "2023-07-24T13:01:08+02:00",
+          "tree_id": "fea67a4caa91e39fcd6aefa429ac91079dee0f68",
+          "url": "https://github.com/scolladon/sfdx-git-delta/commit/0f8a869b0f7d07dbd28d04c0222bbae2377f9758"
+        },
+        "date": 1690196952816,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "e2e-test",
+            "value": 0.12,
+            "range": "±5.37%",
+            "unit": "ops/sec",
+            "extra": "5 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
