5bfbdac7cc20ef72f3d5e617f40b978fb7951217
Sebastien <colladonsebastien@gmail.com>
2023-07-25 14:41:14 +0200

    test: improve NUT test with E2E tests (#659)

--- __tests__/functional/delta.nut.ts
+++ __tests__/functional/delta.nut.ts
@@ -1,6 +1,8 @@
 'use strict'
 const { execCmd } = require('@salesforce/cli-plugins-testkit')
 const { expect } = require('@salesforce/command/lib/test')
+const readline = require('readline')
+const fs = require('fs')
 
 describe('sgd:source:delta NUTS', () => {
   it('run help', () => {
@@ -41,4 +43,36 @@ describe('sgd:source:delta NUTS', () => {
     expect(result).to.include('"error": null')
     expect(result).to.include('"success": true')
   })
+
+  it('run `e2e` tests', async () => {
+    // Act
+    const result = execCmd(
+      'sgd:source:delta --from "origin/e2e/base" --to "origin/e2e/head" --output e2e/expected --generate-delta --repo e2e --ignore e2e/.sgdignore',
+      {
+        ensureExitCode: 0,
+      }
+    ).shellOutput
+
+    // Assert
+    const packageLineCount = await getFileLineNumber(
+      'e2e/expected/package/package.xml'
+    )
+    expect(packageLineCount).to.equal(194)
+    expect(result).to.include('"error": null')
+    expect(result).to.include('"success": true')
+  })
 })
+
+const getFileLineNumber = async path => {
+  let linesCount = 0
+  const rl = readline.createInterface({
+    input: fs.createReadStream(path),
+    output: process.stdout,
+    terminal: false,
+  })
+  // eslint-disable-next-line no-unused-vars
+  for await (const _ of rl) {
+    ++linesCount
+  }
+  return linesCount
+}
