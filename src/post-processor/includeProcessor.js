c2ce3a28c1801928215080726c1d5a6e8d336830
Sébastien Colladon <colladonsebastien@gmail.com>
2023-08-27 23:11:50 +0200

    refactor: improve readibility

--- src/post-processor/includeProcessor.js
+++ src/post-processor/includeProcessor.js
@@ -9,6 +9,8 @@ const TAB = '\t'
 
 class IncludeProcessor extends BaseProcessor {
   gitHelper
+  from
+  includeHelper
   constructor(work, metadata) {
     super(work, metadata)
     this.gitHelper = new RepoSetup(this.config)
@@ -16,47 +18,64 @@ class IncludeProcessor extends BaseProcessor {
 
   async process() {
     if (this._shouldProcess()) {
-      // *****Setup
-      const from = this.config.from
-      const firstSha = await this.gitHelper.getFirstCommitRef()
-      this.config.from = firstSha
-
-      const includeHelper = await buildIncludeHelper(this.config)
-
-      // ******Execution
-      const linesStream = await this.gitHelper.getAllFilesAsLineStream()
-      const includeHolder = {
-        [ADDITION]: [],
-        [DELETION]: [],
-      }
-      for await (const line of linesStream) {
-        Object.keys(includeHolder).forEach(changeType => {
-          const changedLine = `${changeType}${TAB}${treatPathSep(line)}`
-          if (!includeHelper.keep(changedLine)) {
-            includeHolder[changeType].push(changedLine)
-          }
-        })
-      }
-
-      if (includeHolder[ADDITION].length > 0) {
-        const lineProcessor = new DiffLineInterpreter(this.work, this.metadata)
-        await lineProcessor.process(includeHolder[ADDITION])
-      }
-
-      if (includeHolder[DELETION].length > 0) {
-        const to = this.config.to
-        this.config.to = this.config.from
-        this.config.from = to
-        const lineProcessor = new DiffLineInterpreter(this.work, this.metadata)
-        await lineProcessor.process(includeHolder[DELETION])
-        this.config.to = to
-      }
-      this.config.from = from
+      await this._prepare()
+      await this._process()
+      this._cleanup()
     }
   }
 
   _shouldProcess() {
     return !!this.config.include || !!this.config.includeDestructive
+  }
+
+  async _prepare() {
+    this.from = this.config.from
+    const firstSha = await this.gitHelper.getFirstCommitRef()
+    this.config.from = firstSha
+
+    this.includeHelper = await buildIncludeHelper(this.config)
+  }
+
+  async _process() {
+    const includeHolder = {
+      [ADDITION]: [],
+      [DELETION]: [],
+    }
+    const linesStream = await this.gitHelper.getAllFilesAsLineStream()
+    for await (const line of linesStream) {
+      Object.keys(includeHolder).forEach(changeType => {
+        const changedLine = `${changeType}${TAB}${treatPathSep(line)}`
+        if (!this.includeHelper.keep(changedLine)) {
+          includeHolder[changeType].push(changedLine)
+        }
+      })
+    }
+
+    if (includeHolder[ADDITION].length > 0) {
+      await this._processInclude(includeHolder[ADDITION])
+    }
+
+    if (includeHolder[DELETION].length > 0) {
+      await this._processIncludeDestructive(includeHolder[DELETION])
+    }
+  }
+
+  async _processInclude(lines) {
+    const lineProcessor = new DiffLineInterpreter(this.work, this.metadata)
+    await lineProcessor.process(lines)
+  }
+
+  async _processIncludeDestructive(lines) {
+    const to = this.config.to
+    this.config.to = this.config.from
+    this.config.from = to
+    const lineProcessor = new DiffLineInterpreter(this.work, this.metadata)
+    await lineProcessor.process(lines)
+    this.config.to = to
+  }
+
+  async _cleanup() {
+    this.config.from = this.from
   }
 }
 
