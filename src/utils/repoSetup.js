a79be9a1ceb286f2c0d48cfeeb3b4c43df2fe3ec
Sébastien Colladon <colladonsebastien@gmail.com>
2023-08-27 23:00:29 +0200

    feat: add a method to get all file in the repo at ref

--- src/utils/repoSetup.js
+++ src/utils/repoSetup.js
@@ -1,10 +1,11 @@
 'use strict'
-const { getStreamContent } = require('./childProcessUtils')
+const { getStreamContent, linify } = require('./childProcessUtils')
 const { GIT_COMMAND, UTF8_ENCODING } = require('./gitConstants')
 const { spawn } = require('child_process')
 
 const commitCheckParams = ['cat-file', '-t']
 const firstCommitParams = ['rev-list', '--max-parents=0', 'HEAD']
+const allFilesParams = ['ls-tree', '--name-only', '-r']
 const gitConfig = ['config', 'core.quotepath', 'off']
 
 class RepoSetup {
@@ -37,6 +38,16 @@ class RepoSetup {
     )
 
     return data?.toString(UTF8_ENCODING)
+  }
+
+  async getAllFilesAsLineStream() {
+    const gitLs = spawn(
+      GIT_COMMAND,
+      [...allFilesParams, this.config.to],
+      this.spawnConfig
+    )
+
+    return linify(gitLs.stdout)
   }
 }
 
