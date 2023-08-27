f2ee84b89903031bfd4081354364311b4625cf12
Sébastien Colladon <colladonsebastien@gmail.com>
2023-08-28 01:02:28 +0200

    test: update perf test to use include and ignore

--- __tests__/perf/bench.js
+++ __tests__/perf/bench.js
@@ -6,7 +6,7 @@ const suite = new Benchmark.Suite()
 suite
   .add('e2e-test', () => {
     execCmd(
-      'sgd:source:delta --from "origin/e2e/base" --to "origin/e2e/head" --output e2e/expected --generate-delta --repo e2e',
+      'sgd:source:delta --from "origin/e2e/base" --to "origin/e2e/head" --output e2e/expected --generate-delta --repo e2e --include e2e/.sgdinclude --include-destructive e2e/.sgdincludeDestructive --ignore e2e/.sgdignore --ignore-destructive e2e/.sgdignoreDestructive',
       {
         ensureExitCode: 0,
       }
