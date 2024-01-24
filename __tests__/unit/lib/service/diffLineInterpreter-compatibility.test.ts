e6d6e346a4d794e8b22a7d04224cb9858af5ec23
Sebastien <colladonsebastien@gmail.com>
2024-01-24 10:48:08 +0100

    fix: os api availability issue (#764)
    * fix: os api availability issue
    
    * test: add compatibility test
    
    * build: upgrade dependencies

---
+++ __tests__/unit/lib/service/diffLineInterpreter-compatibility.test.ts
@@ -0,0 +1,55 @@
+'use strict'
+
+import { expect, jest, describe, it } from '@jest/globals'
+import { getGlobalMetadata, getWork } from '../../../__utils__/globalTestHelper'
+import DiffLineInterpreter from '../../../../src/service/diffLineInterpreter'
+import { Work } from '../../../../src/types/work'
+import { MetadataRepository } from '../../../../src/metadata/MetadataRepository'
+
+jest.mock('os', () => ({
+  availableParallelism: null,
+}))
+
+const mockHandle = jest.fn()
+jest.mock('../../../../src/service/typeHandlerFactory', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      getTypeHandler: jest
+        .fn()
+        .mockImplementation(() => ({ handle: mockHandle })),
+    }
+  })
+})
+
+let work: Work
+beforeEach(() => {
+  jest.clearAllMocks()
+  work = getWork()
+})
+
+describe('DiffLineInterpreter', () => {
+  let sut: DiffLineInterpreter
+  let globalMetadata: MetadataRepository
+  beforeAll(async () => {
+    // eslint-disable-next-line no-undef
+    globalMetadata = await getGlobalMetadata()
+  })
+
+  describe('compatibility test', () => {
+    beforeEach(() => {
+      sut = new DiffLineInterpreter(work, globalMetadata)
+    })
+    describe('when `availableParallelism` is not defined', () => {
+      it('fallback gracefully', async () => {
+        // Arrange
+        const lines = ['test']
+
+        // Act
+        await sut.process(lines)
+
+        // Assert
+        expect(mockHandle).toBeCalledTimes(lines.length)
+      })
+    })
+  })
+})
--- src/service/diffLineInterpreter.ts
+++ src/service/diffLineInterpreter.ts
@@ -6,8 +6,6 @@ import { availableParallelism } from 'os'
 import { queue } from 'async'
 import StandardHandler from './standardHandler'
 
-const MAX_PARALLELISM = Math.min(availableParallelism(), 6)
-
 export default class DiffLineInterpreter {
   constructor(
     // eslint-disable-next-line no-unused-vars
@@ -18,6 +16,7 @@ export default class DiffLineInterpreter {
 
   public async process(lines: string[]) {
     const typeHandlerFactory = new TypeHandlerFactory(this.work, this.metadata)
+    const MAX_PARALLELISM = this.getConcurrencyThreshold()
     const processor = queue(
       async (handler: StandardHandler) => await handler.handle(),
       MAX_PARALLELISM
@@ -31,5 +30,14 @@ export default class DiffLineInterpreter {
     if (processor.length() > 0) {
       await processor.drain()
     }
+  }
+
+  protected getConcurrencyThreshold() {
+    // This is because of this issue: https://github.com/scolladon/sfdx-git-delta/issues/762#issuecomment-1907609957
+    const AVAILABLE_PARALLELISM = availableParallelism
+      ? availableParallelism()
+      : Infinity
+
+    return Math.min(AVAILABLE_PARALLELISM, 6)
   }
 }
