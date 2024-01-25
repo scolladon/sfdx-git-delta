e8cd84880a0c87fab0a9c63ab49793338771c39e
Sebastien <colladonsebastien@gmail.com>
2024-01-25 11:45:12 +0100

    fix: use constant for path separator (#766)

--- src/constant/fsConstants.ts
+++ src/constant/fsConstants.ts
@@ -1,8 +1,6 @@
 'use strict'
 
-import { posix } from 'path'
-
 export const DOT = '.'
 export const UTF8_ENCODING = 'utf8'
 export const PATH_SEPARATOR_REGEX = /[/\\]+/
-export const PATH_SEP = posix.sep
+export const PATH_SEP = '/'
