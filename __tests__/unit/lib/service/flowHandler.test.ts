8de33f93639d2f38f54cc488ffb9816b808c414b
Sébastien Colladon <colladonsebastien@gmail.com>
2024-07-24 18:29:38 +0200

    feat: create FlowHandler and warn when deleted

---
+++ __tests__/unit/lib/service/flowHandler.test.ts
@@ -0,0 +1,49 @@
+'use strict'
+import { expect, jest, describe, it } from '@jest/globals'
+
+import { DELETION } from '../../../../src/constant/gitConstants'
+import { MetadataRepository } from '../../../../src/metadata/MetadataRepository'
+import FlowHandler from '../../../../src/service/flowHandler'
+import type { Work } from '../../../../src/types/work'
+import { getGlobalMetadata, getWork } from '../../../__utils__/globalTestHelper'
+
+jest.mock('../../../../src/utils/fsHelper')
+
+const objectType = {
+  directoryName: 'flows',
+  inFolder: false,
+  metaFile: false,
+  suffix: 'flow',
+  xmlName: 'Flow',
+}
+const basePath = `force-app/main/default/${objectType.directoryName}`
+let work: Work
+beforeEach(() => {
+  jest.clearAllMocks()
+  work = getWork()
+})
+
+describe('flowHandler', () => {
+  let globalMetadata: MetadataRepository
+  beforeAll(async () => {
+    globalMetadata = await getGlobalMetadata()
+  })
+  describe('when a flow is deleted', () => {
+    it('warns the user not to', async () => {
+      // Arrange
+      const sut = new FlowHandler(
+        `${DELETION}       ${basePath}/MyFlow.${objectType.suffix}-meta.xml`,
+        objectType,
+        work,
+        globalMetadata
+      )
+      expect(work.warnings.length).toBe(0)
+
+      // Act
+      await sut.handle()
+
+      // Assert
+      expect(work.warnings.length).toBe(1)
+    })
+  })
+})
