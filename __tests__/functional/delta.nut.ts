eb7160a118e4cc7d1aeb130b5b7be330ada3fa38
Sébastien Colladon <colladonsebastien@gmail.com>
2024-09-11 18:43:44 +0200

    test: fix nut test with esm

--- __tests__/functional/delta.nut.ts
+++ __tests__/functional/delta.nut.ts
@@ -3,7 +3,7 @@ import fs from 'node:fs'
 import readline from 'readline'
 
 import { execCmd } from '@salesforce/cli-plugins-testkit'
-import { expect } from '@salesforce/command/lib/test'
+import { expect } from 'chai'
 
 describe('sgd:source:delta NUTS', () => {
   it('run help', () => {
@@ -14,7 +14,7 @@ describe('sgd:source:delta NUTS', () => {
   })
 
   it('detects not existing output folder', () => {
-    const result = execCmd('sgd:source:delta --from "HEAD"', {
+    const result = execCmd('sgd:source:delta --from "HEAD" --json', {
       ensureExitCode: 1,
     }).shellOutput
     expect(result).to.include('folder does not exist')
@@ -30,7 +30,7 @@ describe('sgd:source:delta NUTS', () => {
   })
 
   it('outputs json', () => {
-    const result = execCmd('sgd:source:delta --from "HEAD" -o reports', {
+    const result = execCmd('sgd:source:delta --from "HEAD" -o reports --json', {
       ensureExitCode: 0,
     }).shellOutput
     expect(result).to.include('"error": null')
@@ -48,7 +48,7 @@ describe('sgd:source:delta NUTS', () => {
   it('run `e2e` tests', async () => {
     // Act
     const result = execCmd(
-      'sgd:source:delta --from "origin/e2e/base" --to "origin/e2e/head" --output e2e/expected --generate-delta --repo e2e --include e2e/.sgdinclude --include-destructive e2e/.sgdincludeDestructive --ignore e2e/.sgdignore --ignore-destructive e2e/.sgdignoreDestructive',
+      'sgd:source:delta --from "origin/e2e/base" --to "origin/e2e/head" --output e2e/expected --generate-delta --repo e2e --include e2e/.sgdinclude --include-destructive e2e/.sgdincludeDestructive --ignore e2e/.sgdignore --ignore-destructive e2e/.sgdignoreDestructive --json',
       {
         ensureExitCode: 0,
       }
