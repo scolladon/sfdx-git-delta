e1ddbe19a4e0535b89e9593795bda2c8596facc5
Sébastien Colladon <colladonsebastien@gmail.com>
2024-09-12 15:22:52 +0200

    build: add plugin linting

--- bin/run.js
+++ bin/run.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-
-// eslint-disable-next-line node/shebang
 async function main() {
-  const { execute } = await import('@oclif/core');
-  await execute({ dir: import.meta.url });
+  const { execute } = await import('@oclif/core')
+  await execute({ dir: import.meta.url })
 }
 
-await main();
+await main()
