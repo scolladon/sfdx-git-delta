1ee7c40cef1db310fa55439bc69afc97dae4b2c8
Sébastien Colladon <colladonsebastien@gmail.com>
2024-09-16 18:43:00 +0200

    refactor: use Message for every errors and warnings

--- __tests__/functional/delta.nut.ts
+++ __tests__/functional/delta.nut.ts
@@ -10,39 +10,7 @@ describe('sgd source delta NUTS', () => {
     const result = execCmd('sgd source delta --help', {
       ensureExitCode: 0,
     }).shellOutput
-    expect(result).to.include('sgd source delta')
-  })
-
-  it('detects not existing output folder', () => {
-    const result = execCmd('sgd source delta --from "HEAD" --json', {
-      ensureExitCode: 1,
-    }).shellOutput
-    expect(result).to.include('folder does not exist')
-    expect(result).to.include('"success": false')
-  })
-
-  it('detects not existing output folder with json outputs', () => {
-    const result = execCmd('sgd source delta --from "HEAD" --json', {
-      ensureExitCode: 1,
-    }).shellOutput
-    expect(result).to.include('folder does not exist')
-    expect(result).to.include('"success": false')
-  })
-
-  it('outputs json', () => {
-    const result = execCmd('sgd source delta --from "HEAD" -o reports --json', {
-      ensureExitCode: 0,
-    }).shellOutput
-    expect(result).to.include('"error": null')
-    expect(result).to.include('"success": true')
-  })
-
-  it('outputs json with `--json`', () => {
-    const result = execCmd('sgd source delta --from "HEAD" -o reports --json', {
-      ensureExitCode: 0,
-    }).shellOutput
-    expect(result).to.include('"error": null')
-    expect(result).to.include('"success": true')
+    expect(result).to.include('incremental')
   })
 
   it('run `e2e` tests', async () => {
