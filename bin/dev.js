d7048222bf6aa47ba271b9019bddcce35c47db97
Sébastien Colladon <colladonsebastien@gmail.com>
2024-10-05 14:02:37 +0200

    fix: dev script with ts-node

--- bin/dev.js
+++ bin/dev.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
+#!/usr/bin/env -S NODE_OPTIONS="--no-warnings=ExperimentalWarning" yarn ts-node --project tsconfig.json --esm
 async function main() {
   const { execute } = await import('@oclif/core')
   await execute({ development: true, dir: import.meta.url })
