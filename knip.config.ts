83a26ded042025d418052e69c96a3af6d3378e72
Sébastien Colladon <colladonsebastien@gmail.com>
2024-10-06 15:16:59 +0200

    build: fix dependency linting config file format issue

--- knip.config.ts
+++ knip.config.ts
@@ -1,26 +1,20 @@
-import type { KnipConfig } from 'knip'
-
-const config = async (): Promise<KnipConfig> => {
-  return {
-    entry: [
-      'src/commands/sgd/source/delta.ts',
-      'src/main.ts',
-      'bin/dev.js',
-      'bin/run.js',
-    ],
-    project: ['src/**/*.ts', 'bin/*.js'],
-    ignoreDependencies: [
-      '@salesforce/ts-sinon',
-      '@types/chai',
-      '@types/jest',
-      '@types/mocha',
-      'chai',
-      'mocha',
-      'sinon',
-      'ts-jest-mock-import-meta',
-    ],
-    ignoreBinaries: ['test:e2e'],
-  }
+export default {
+  packageManager: 'npm',
+  entry: [
+    'src/commands/sgd/source/delta.ts',
+    'src/main.ts',
+    'bin/dev.js',
+    'bin/run.js',
+    '**/*.{nut,test}.ts',
+    '.github/**/*.yml',
+  ],
+  project: ['**/*.{ts,js,json,yml}'],
+  ignoreDependencies: [
+    '@salesforce/ts-sinon',
+    '@types/mocha',
+    'mocha',
+    'sinon',
+    'ts-jest-mock-import-meta',
+  ],
+  ignoreBinaries: ['npm-check-updates'],
 }
-
-export default config
