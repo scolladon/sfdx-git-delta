e98027ba365fb823dca6fee2e28a9881873938c5
Sébastien Colladon <colladonsebastien@gmail.com>
2024-11-08 09:06:23 +0100

    fix: not necessary path handling

--- src/adapter/GitAdapter.ts
+++ src/adapter/GitAdapter.ts
@@ -104,7 +104,7 @@ export default class GitAdapter {
         oid: this.config.to,
       })
       bufferFiles.push({
-        path: treatPathSep(filePath),
+        path: filePath,
         content: fileContent,
       })
     }
