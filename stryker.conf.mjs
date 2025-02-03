91afa4a6639866633703fae0612be8e852bd933a
Sébastien Colladon <colladonsebastien@gmail.com>
2025-02-03 11:27:12 +0100

    build: remove clear-text mutation testing reporter

--- stryker.conf.mjs
+++ stryker.conf.mjs
@@ -2,7 +2,7 @@ const config = {
   coverageAnalysis: 'perTest',
   ignorePatterns: ['lib/', 'reports/', 'bin/', 'e2e/'],
   mutate: ['src/**/*.ts', '!src/metadata/v*.ts'],
-  reporters: ['html', 'clear-text', 'progress'],
+  reporters: ['html', 'progress'],
   testRunner: 'jest',
 }
 export default config
