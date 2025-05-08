3e5b6cb44ebca1fc9ffa77bccbfeb372fc660db8
Sébastien Colladon <colladonsebastien@gmail.com>
2025-05-09 08:47:35 +0900

    fix: wrong suffix for DataKitObjectTemplate

--- src/metadata/v59.ts
+++ src/metadata/v59.ts
@@ -10,7 +10,7 @@ export default [
     directoryName: 'dataKitObjectTemplates',
     inFolder: false,
     metaFile: false,
-    suffix: 'dataStreamdataKitObjectTemplateTemplate',
+    suffix: 'dataKitObjectTemplate',
     xmlName: 'DataKitObjectTemplate',
   },
   {
