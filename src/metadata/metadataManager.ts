45cb016b41a9cc4d686404fba519d998a655aadc
Sébastien Colladon <colladonsebastien@gmail.com>
2025-09-08 18:17:34 +0200

    feat: add Winter '26 (v65) release preview support

--- src/metadata/metadataManager.ts
+++ src/metadata/metadataManager.ts
@@ -13,7 +13,7 @@ const inFileMetadata = new Map<string, SharedFileMetadata>()
 const sharedFolderMetadata = new Map<string, string>()
 
 const earliestVersion: number = 46
-const latestVersion: number = 64
+const latestVersion: number = 65
 
 export const getLatestSupportedVersion = () => {
   return latestVersion - 1
